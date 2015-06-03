ab7f89c04a7003811514e5e90c6b7d7baa1d2b55
Lev-Urbansky <nickolas.konnors@gmail.com>
2015-06-03 15:22:24 +0300

    Area now computing figures

--- src/Area.java
+++ src/Area.java
@@ -1,39 +1,46 @@
 /**
-<<<<<<< Updated upstream
- * Created by ��� on 03.06.2015.
-=======
+
  * Created by Лев on 03.06.2015.
->>>>>>> Stashed changes
+
  */
 public class Area {
     private double X;
     private double Y;
-    private double Z;
+    private double radius;
+    public enum GeomFigType {RECTANGLE, CIRCLE};
+    public Area(double X, double Y, double radius) {
+        switch (GeomFigType){
+            case RECTANGLE:
+                this.X = X;
+                this.Y = Y;
+                break;
+            case CIRCLE:
+                this.radius = radius;
+                break;
+        }
 
-    public Area(double X, double Y, double Z) {
-        this.X = X;
-        this.Y = Y;
-        this.Z = Z;
     }
 
 
 
-    public double getSphereArea() {
-        return X * Y * Z;
+    public double getArea() {
+        switch (GeomFigType){
+            case RECTANGLE:
+               return X * Y;
+                break;
+            case CIRCLE:
+               return Math.PI * radius * radius;
+                break;
+            default:
+                System.out.println("У данной фигуры площадь не считается");
+        }
+        return 0;
     }
 
 
-    public double getParelpArea() {
-        return X * Y * Z;
-    }
-
-
-    public double getSquareArea() {
-        return X * Y * Z;
-    }
 
     public static void main(String[] args) {
         Area area = new Area(11.4, 18.6, 16.7);
-        System.out.println(area.getSquareArea());
+        System.out.println(area.getArea());
     }
 }
