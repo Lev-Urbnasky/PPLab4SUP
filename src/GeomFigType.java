5b8f5301e325a24b13ea5d9d9c35c5e4eab901bf
GsomGsom <bit8>
2015-06-03 14:32:19 +0300

    Created enumeration for figures.

---
+++ src/GeomFigType.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Igor on 03/06/15.
+ */
+public enum GeomFigType {
+    SPHERE, CONE, PARALP, PYRAMIDE, RECTANGLE, TRIANGLE, CIRCLE
+}
---
+++ src/Surface.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Igor on 03/06/15.
+ */
+public class Surface {
+
+    public static void main (String[] args) {
+
+    }
+}
