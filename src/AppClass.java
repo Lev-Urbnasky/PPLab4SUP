c121fa3566d3cacf0e61a1d194a9c0cf615ed4ac
GsomGsom <bit8>
2015-06-04 22:09:18 +0300

    GeomFigure: -setter for GeomFigureType. AppClass: -class for app created.

--- src/AppClass.java
+++ src/AppClass.java
@@ -1,63 +1,81 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+import java.util.Scanner;
 
-public class AppClass extends JFrame {
-    private String [] listOfFigures = {GeomFigType.SPHERE.toString(), GeomFigType.CONE.toString(),
-            GeomFigType.PARALP.toString(), GeomFigType.RECTANGLE.toString(), GeomFigType.CIRCLE.toString()};
-
-    private GeomFigure currentFigure;
-
-    private JPanel leftPanel = new JPanel();
-    private JComboBox box = new JComboBox(listOfFigures);
-    private JButton calucalteButton = new JButton("Calculate!");
-
-    private JPanel rightPanel = new JPanel();
-    private JTextField lengthInput = new JTextField(10);
-    private JLabel lengthInputLabel = new JLabel();
-    private JTextField widthInput = new JTextField(10);
-    private JLabel widthLabel = new JLabel();
-    private JTextField heigthInput = new JTextField(10);
-    private JLabel heigthLabel = new JLabel();
-
-    private JTextField leftResultField = new JTextField(10);
-    private JLabel leftResultLabel = new JLabel();
-
-    private JTextField rightResultField = new JTextField(10);
-    private JLabel rightResultLabel = new JLabel();
-
-    AppClass() {
-        this.setTitle("Super Mega Cool Lab App for PP");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.getContentPane().setLayout(new GridLayout(2,2));
-
-        this.leftPanel.setLayout(new GridLayout(2,1));
-        this.leftPanel.add(box);
-        this.leftPanel.add(calucalteButton);
-
-        this.getContentPane().add(this.leftPanel);
-
-        this.box.addItemListener(
-                new ItemListener() {
-                    @Override
-                    public void itemStateChanged(ItemEvent e) {
-                        if (e.getStateChange() == ItemEvent.SELECTED) {
-
-                        }
-                    }
-                }
-        );
-        //this.add(box);
-        this.currentFigure = new GeomFigure(GeomFigType.SPHERE);
-        this.currentFigure.setHeigth(0);
-        this.currentFigure.setLength(0);
-        this.currentFigure.setWidth(0);
-    }
-
+public class AppClass {
     public static void main (String[] args) {
-        AppClass app = new AppClass();
-        app.pack();
-        app.setVisible(true);
+        Scanner scanner = new Scanner(System.in, "UTF-8");
+        int choice = 0;
+        GeomFigure figure = new GeomFigure(GeomFigType.UNDEF);
+
+        System.out.println("Программа для подсчета площади поверхности/площади/периметра разных геометрических фигур.");
+        System.out.println("Выберите фигуру:\n");
+        System.out.println("1. Сфера");
+        System.out.println("2. Конус");
+        System.out.println("3. Параллелепипед");
+        System.out.println("4. Прямоугольник");
+        System.out.println("5. Круг");
+        choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+                figure.setGFType(GeomFigType.SPHERE);
+                break;
+            case 2:
+                figure.setGFType(GeomFigType.CONE);
+                break;
+            case 3:
+                figure.setGFType(GeomFigType.PARALP);
+                break;
+            case 4:
+                figure.setGFType(GeomFigType.RECTANGLE);
+                break;
+            case 5:
+                figure.setGFType(GeomFigType.CIRCLE);
+                break;
+            default:
+                System.out.println("Не правильно введен номер.");
+                break;
+        }
+
+        switch (figure.whatType()) {
+            case SPHERE:
+                System.out.print("Введите радиус сферы: ");
+                figure.setLength(scanner.nextDouble());
+                System.out.println("Площадь поверхности: " + Surface.getFigSurface(figure));
+                break;
+            case CONE:
+                System.out.print("Введите радиус основания конуса: ");
+                figure.setLength(scanner.nextDouble());
+                System.out.print("Введите высоту конуса: ");
+                figure.setHeigth(scanner.nextDouble());
+                System.out.println("Площадь поверхности: " + Surface.getFigSurface(figure));
+                break;
+            case PARALP:
+                System.out.print("Введите длину: ");
+                figure.setLength(scanner.nextDouble());
+                System.out.print("Введите ширину: ");
+                figure.setWidth(scanner.nextDouble());
+                System.out.print("Введите высоту: ");
+                figure.setHeigth(scanner.nextDouble());
+                System.out.println("Площадь поверхности: " + Surface.getFigSurface(figure));
+                /* TODO для периметра */
+                //System.out.println("Периметр: " + ...);
+                break;
+            case RECTANGLE:
+                System.out.print("Введите длину: ");
+                figure.setLength(scanner.nextDouble());
+                System.out.print("Введите ширину: ");
+                figure.setWidth(scanner.nextDouble());
+                /* TODO для периметра и площади */
+                //System.out.println("Периметр: " + ...);
+                //System.out.println("Площадь: " + ...);
+                break;
+            case CIRCLE:
+                System.out.print("Введите радиус круга: ");
+                figure.setLength(scanner.nextDouble());
+                /* TODO для периметра (длины окружности)  и площади */
+                //System.out.println("Периметр: " + ...);
+                //System.out.println("Площадь: " + ...);
+                break;
+        }
+
     }
 }
